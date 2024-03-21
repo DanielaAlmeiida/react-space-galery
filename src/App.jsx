00080d3fbfdb57a7405e3a8f3af4e852d19d93cc
Daniela Almeida <danielaspbrasil@gmail.com>
2024-03-21 12:14:53 -0300

    Criada barra lateral e adicionada fonte

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { styled } from "styled-components"
 import EstilosGlobais from "./components/EstilosGlobais"
 import Cabecalho from "./components/Cabecalho"
+import BarraLateral from "./components/BarraLateral"
+import Banner from "./components/Banner"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -15,6 +17,12 @@ function App() {
     <FundoGradiente>
       <EstilosGlobais />
       <Cabecalho />
+      <BarraLateral />
+      <Banner
+        texto = "oi"
+        backgroundImage = "./imagens/galeria/foto-1.png"
+      >
+      </Banner>
     </FundoGradiente>
   )
 
