a5b6e3aacdd7b15fa24efba64f96bc2c53837891
Daniela Almeida <danielaspbrasil@gmail.com>
2024-04-05 11:56:22 -0300

    Feito componente de imagem

--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components"
 import Titulo from "../Titulo"
 import Tags from "./Tags"
 import Populares from "./Populares"
+import Imagem from "./Imagem"
 
 const GaleriaContainer = styled.div`
     display: flex;
@@ -11,17 +12,28 @@ const SecaoFluida = styled.section`
     flex-grow: 1;
 `
 
-const Galeria = () => {
+const ImagensContainer = styled.section`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 24px;
+`
+
+const Galeria = ({ fotos = [] }) => {
     return (
         <>
             <GaleriaContainer>
                 <SecaoFluida>
                     <Tags />
                     <Titulo>Navegue pela galeria</Titulo>
+                        <ImagensContainer>
+                            {fotos.map(foto => <Imagem
+                                key={foto.id}
+                                foto={foto} />)
+                            }
+                        </ImagensContainer>
                 </SecaoFluida>
-                <Populares>
-
-                </Populares>
+                <Populares />
             </GaleriaContainer>
         </>
     )
